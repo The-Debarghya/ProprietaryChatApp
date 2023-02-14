11786d202c07f5a7c6ce1a2c4fc5133880e60e05
The-Debarghya <debarghya4444@gmail.com>
2023-02-14 23:21:26 +0530

    Add react

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import express from "express";
-import * as dotenv from "../node_modules/dotenv/lib/main";
+import * as dotenv from "dotenv";
+
+import chats from "./data/data.js";
 dotenv.config();
 
 const app: express.Application = express()
@@ -9,6 +11,15 @@ const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
     res.send("express typescript")
+})
+
+app.get('/api/chat', (req: Request, res: Response) => {
+    res.send(chats);
+})
+
+app.get('/api/chat/:id', (req: Request, res: Response) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat)
 })
 
 app.listen(port, (): void => {
